ba85151042067488c9df367a9d2b8ef4447aea6b
jedbenslama <jed.benslama@student.junia.com>
2025-05-12 11:58:29 +0200

    ajout allocation dynamique de memoire avec fonction ajout de plateformes + debut main menu avec plateformes comme boutons

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-#define GRAVITY 0.5
-#define JUMP_VELOCITY -10
-#define PLAYER_SPEED 5
-
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "objets.h"
-
 
 typedef struct {
     SDL_Rect rect;
@@ -16,10 +10,79 @@ typedef struct {
     bool onGround;
 } Player;
 
-int main() {
+typedef struct {
+    SDL_Rect* array;
+    int count;
+    int capacity;
+} PlatformList;
+
+typedef struct {
+    SDL_Rect* array;
+    int count;
+    int capacity;
+} ButtonList;
+
+void initButtonList(ButtonList* list) {
+    list->count = 0;
+    list->capacity = 2;
+    list->array = malloc(sizeof(SDL_Rect) * list->capacity);
+}
+
+void addButton(ButtonList* list, int x, int y, int w, int h) {
+    if (list->count >= list->capacity) {
+        list->capacity *= 2;
+        list->array = realloc(list->array, sizeof(SDL_Rect) * list->capacity);
+    }
+    list->array[list->count++] = (SDL_Rect){ x, y, w, h };
+}
+
+void renderButtons(SDL_Renderer* renderer, SDL_Texture* texture, ButtonList* list, float cameraX) {
+    for (int i = 0; i < list->count; i++) {
+        SDL_Rect dst = list->array[i];
+        dst.x -= (int)cameraX;
+        SDL_RenderCopy(renderer, texture, NULL, &dst);
+    }
+}
+
+void freeButtonList(ButtonList* list) {
+    free(list->array);
+    list->array = NULL;
+    list->count = 0;
+    list->capacity = 0;
+}
+
+void initPlatformList(PlatformList* list) {
+    list->count = 0;
+    list->capacity = 4;
+    list->array = malloc(sizeof(SDL_Rect) * list->capacity);
+}
+
+void addPlatform(PlatformList* list, int x, int y, int w, int h) {
+    if (list->count >= list->capacity) {
+        list->capacity *= 2;
+        list->array = realloc(list->array, sizeof(SDL_Rect) * list->capacity);
+    }
+    list->array[list->count++] = (SDL_Rect){ x, y, w, h };
+}
+
+void renderPlatforms(SDL_Renderer* renderer, SDL_Texture* texture, PlatformList* list, float cameraX) {
+    for (int i = 0; i < list->count; i++) {
+        SDL_Rect dst = list->array[i];
+        dst.x -= (int)cameraX;
+        SDL_RenderCopy(renderer, texture, NULL, &dst);
+    }
+}
+
+void freePlatformList(PlatformList* list) {
+    free(list->array);
+    list->array = NULL;
+    list->count = 0;
+    list->capacity = 0;
+}
+
+void startGame() {
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
-
     SDL_Window* window = SDL_CreateWindow("Platformer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
@@ -27,38 +90,40 @@ int main() {
     SDL_Texture* brick = IMG_LoadTexture(renderer, "assets/brick.jpg");
     SDL_Texture* soltexture = IMG_LoadTexture(renderer, "assets/soltexture.jpg");
     SDL_Texture* character = IMG_LoadTexture(renderer, "assets/personnage_d.png");
-    
-    // x y largeur hauteur
+    SDL_Texture* startButton = IMG_LoadTexture(renderer, "assets/startButton.png");
 
-    SDL_Rect platform = { 500, 400, 200, 50 };
-    SDL_Rect platform2 = { 200, 400, 80, 300 };
+
+    PlatformList platforms;
+    initPlatformList(&platforms);
+    addPlatform(&platforms, 500, 400, 200, 50);
+    addPlatform(&platforms, 200, 400, 80, 300);
+    addPlatform(&platforms, 300, 300, 100, 100);
+
+    ButtonList buttons;
+    initButtonList(&buttons);
+    addButton(&buttons, 700, SCREEN_HEIGHT - 200, 80, 50); // bouton cliquable
+
 
     Player player = { .rect = {100, SCREEN_HEIGHT - 200, 50, 50}, .velY = 0, .onGround = false };
-
     float cameraX = 0;
-
-    bool running = true;
     SDL_Event event;
+    bool running = true;
 
     while (running) {
-        if(player.rect.y>2000){
-            exit(123);
-        }
+        if (player.rect.y > 2000) exit(123);
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
                 running = false;
         }
+
         const Uint8* keystate = SDL_GetKeyboardState(NULL);
-        if (keystate[SDL_SCANCODE_LEFT]){ player.rect.x -= PLAYER_SPEED; character = IMG_LoadTexture(renderer, "assets/personnage_g.png");}
-        if (keystate[SDL_SCANCODE_RIGHT]){ player.rect.x += PLAYER_SPEED; character = IMG_LoadTexture(renderer, "assets/personnage_d.png");}
+        if (keystate[SDL_SCANCODE_LEFT]) { player.rect.x -= PLAYER_SPEED; character = IMG_LoadTexture(renderer, "assets/personnage_g.png"); }
+        if (keystate[SDL_SCANCODE_RIGHT]) { player.rect.x += PLAYER_SPEED; character = IMG_LoadTexture(renderer, "assets/personnage_d.png"); }
         if ((keystate[SDL_SCANCODE_SPACE] || keystate[SDL_SCANCODE_UP]) && player.onGround) {
             player.velY = JUMP_VELOCITY;
             player.onGround = false;
         }
-        if (keystate[SDL_SCANCODE_G]){ platform.x+=10;}
-        if (keystate[SDL_SCANCODE_F]){ platform.x-=10;}
-        if (keystate[SDL_SCANCODE_R]){ platform.y-=10;}
-        if (keystate[SDL_SCANCODE_T]){ platform.y+=10;}
 
         player.rect.y += player.velY;
         player.velY += GRAVITY;
@@ -67,46 +132,62 @@ int main() {
             player.rect.y = ground.y - player.rect.h;
             player.velY = 0;
             player.onGround = true;
-        }
-        else if (checkCollision(player.rect, platform) && player.velY >= 0 && player.rect.y <= platform.y + 20) {
-            player.rect.y = platform.y - player.rect.h;
-            player.velY = 0;
-            player.onGround = true;
-        }else if (checkCollision(player.rect, platform2) && player.velY >= 0 && player.rect.y <= platform2.y + 20) {
-            player.rect.y = platform2.y - player.rect.h;
-            player.velY = 0;
-            player.onGround = true;
         } else {
-            player.onGround = false;
+            player.onGround = false; // faudra ptt enlever ca
+            for (int i = 0; i < platforms.count; i++) {
+                SDL_Rect plat = platforms.array[i];
+                if (checkCollision(player.rect, plat) && player.velY >= 0 && player.rect.y <= plat.y -30) {
+                    player.rect.y = plat.y - player.rect.h;
+                    player.velY = 0;
+                    player.onGround = true;
+                    break;
+                }
+            }
         }
+
+        for (int i = 0; i < buttons.count; i++) {
+            if (checkCollision(player.rect, buttons.array[i])) {
+
+                if (i == 0) {
+                    freePlatformList(&platforms);
+                    SDL_DestroyTexture(background);
+                    SDL_DestroyTexture(brick);
+                    SDL_DestroyTexture(soltexture);
+                    SDL_DestroyTexture(character);
+                    SDL_DestroyRenderer(renderer);
+                    SDL_DestroyWindow(window);
+                    IMG_Quit();
+                    SDL_Quit();
+
+
+                    startGame();
+                    return;
+                }
+            }
+        }        
 
         cameraX = player.rect.x + player.rect.w / 2 - SCREEN_WIDTH / 2;
         if (cameraX < 0) cameraX = 0;
 
         SDL_RenderClear(renderer);
-
         SDL_RenderCopy(renderer, background, NULL, NULL);
 
-        SDL_Rect groundQuandBouge = ground;
-        groundQuandBouge.x -= (int)cameraX;
-        SDL_RenderCopy(renderer, soltexture, NULL, &groundQuandBouge);
+        SDL_Rect groundMoved = ground;
+        groundMoved.x -= (int)cameraX;
+        SDL_RenderCopy(renderer, soltexture, NULL, &groundMoved);
 
-        SDL_Rect platformQuandBouge = platform;
-        platformQuandBouge.x -= (int)cameraX;
-        SDL_RenderCopy(renderer, brick, NULL, &platformQuandBouge);
+        renderPlatforms(renderer, brick, &platforms, cameraX);
+        renderButtons(renderer, startButton, &buttons, cameraX);
 
-        SDL_Rect platform2QuandBouge = platform2;
-        platform2QuandBouge.x -= (int)cameraX;
-        SDL_RenderCopy(renderer, brick, NULL, &platform2QuandBouge);
-
-        SDL_Rect playerScreenCentre = player.rect;
-        playerScreenCentre.x -= (int)cameraX;
-        SDL_RenderCopy(renderer, character, NULL, &playerScreenCentre);
+        SDL_Rect playerScreen = player.rect;
+        playerScreen.x -= (int)cameraX;
+        SDL_RenderCopy(renderer, character, NULL, &playerScreen);
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(10); // 100 FPS
+        SDL_Delay(10);
     }
 
+    freePlatformList(&platforms);
     SDL_DestroyTexture(background);
     SDL_DestroyTexture(brick);
     SDL_DestroyTexture(soltexture);
@@ -115,6 +196,8 @@ int main() {
     SDL_DestroyWindow(window);
     IMG_Quit();
     SDL_Quit();
+}
 
-    return 0;
+int main() {
+    startGame();
 }
--- objets.h
+++ objets.h
@@ -1,5 +1,19 @@
+#ifndef OBJETS_H
+#define OBJETS_H
+
+#include <SDL.h>
+#include <stdbool.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define GRAVITY 0.5
+#define JUMP_VELOCITY -10
+#define PLAYER_SPEED 5
+
 bool checkCollision(SDL_Rect a, SDL_Rect b) {
     return SDL_HasIntersection(&a, &b);
 }
 
 SDL_Rect ground = { 0, SCREEN_HEIGHT - 100, 2000, 100 };
+
+#endif
